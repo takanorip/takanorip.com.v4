57a15b55b2f0d5e143d4d6c5c497227145104992
takanorip <takanori0170@gmail.com>
2022-01-05 01:33:19 +0900

    add

--- src/components/Activity/data.ts
+++ src/components/Activity/data.ts
@@ -19,5 +19,15 @@ export const activities = [
     title: 'Web Platform Study Group',
     category: 'community',
     link: 'https://www.youtube.com/channel/UCfToJ-sTOqvBnnuVq3zdZhA'
+  },
+  {
+    title: 'devchat.fm',
+    category: 'podcast',
+    link: 'https://anchor.fm/devchat-fm/episodes/27-UI-1-with-takanoripe-et762e/a-a3leebm'
+  },
+  {
+    title: 'Next Web Conf',
+    category: 'conference',
+    link: 'https://youtu.be/ftqAyw8gYNQ'
   }
 ]
