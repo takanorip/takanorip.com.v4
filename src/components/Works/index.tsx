a898ad5df79563a33737f89f2605bab015c18f9f
takanorip <takanori0170@gmail.com>
2022-01-07 03:19:00 +0900

    add

--- src/components/Works/index.tsx
+++ src/components/Works/index.tsx
@@ -1,18 +1,29 @@
 import { h } from 'preact';
-import { useState } from 'preact/hooks';
+import { useState, useEffect } from 'preact/hooks';
 import cx from 'classnames';
 import WorkItem from '../../components/WorkItem/index';
 import Styles from './styles.module.css';
 
 const Works = ({ works }) => {
   const categoryList = ['Design', 'Development', 'Speaking', 'Writing'];
+  const [animate, setAnimate] = useState(false);
   const [filteredWorks, setFilteredWorks] = useState(works);
   const [currentFilter, setCurrentFilter] = useState('All');
-  const filter = (keyword) => {
-    const fw = works.filter(w => w.category.includes(keyword))
-    setFilteredWorks(fw);
-    setCurrentFilter(keyword);
-  }
+
+  useEffect(() => {
+    setAnimate(false);
+    if (currentFilter === 'All') {
+      setFilteredWorks(works);
+    } else {
+      const fw = works.filter(w => w.category.includes(currentFilter));
+      setFilteredWorks(fw);
+    }
+    const timer = setTimeout(() => {
+      setAnimate(true);
+    }, 300);
+    return () => clearTimeout(timer);
+  }, [currentFilter]);
+
   return (
     <div className={Styles.works}>
       <div className={Styles.tabOverflow}>
@@ -22,11 +33,11 @@ const Works = ({ works }) => {
             setCurrentFilter('All');
           }} role="button">All</button>
           {categoryList.map(l => (
-            <button className={cx(Styles.tab, { [Styles.active]: currentFilter === l })} onClick={() => filter(l)} role="button">{l}</button>
+            <button className={cx(Styles.tab, { [Styles.active]: currentFilter === l })} onClick={() => setCurrentFilter(l)} role="button">{l}</button>
           ))}
         </div>
       </div>
-      <div className={Styles.grid}>
+      <div className={cx(Styles.grid, { [Styles.animate]: animate })}>
         {filteredWorks.map(item => <WorkItem className={Styles.item} item={item} />)}
       </div>
     </div>
