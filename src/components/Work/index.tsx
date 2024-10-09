6f1d33e8d6361a6f5eb3fe054f72480e8d918432
takanorip <takanori.oki@dr-ubie.com>
2024-10-10 00:25:35 +0900

    update style

--- src/components/Work/index.tsx
+++ src/components/Work/index.tsx
@@ -6,26 +6,36 @@ import { format, addHours } from "date-fns";
 
 type Props = {
   data: any;
+  spekerdeckInfo: any;
 };
 
-const Work: FC<Props> = ({ data }) => {
+const Work: FC<Props> = ({ data, spekerdeckInfo }) => {
   return (
     <div className={styles.wrapper}>
       <figure className={styles.figure}>
-        <img className={styles.image} src={data.image.url} alt={data.title} />
+        {spekerdeckInfo ? (
+          <iframe
+            className="speakerdeck-iframe"
+            src={spekerdeckInfo.link}
+            title={spekerdeckInfo.title}
+            allowFullScreen
+          />
+        ) : (
+          <img className={styles.image} src={data.image.url} alt={data.title} />
+        )}
       </figure>
       <div className={styles.titleWrapper}>
         <h2 className={styles.title}>{data.title}</h2>
         <div className={styles.info}>
-          <div className={styles.infoDate}>
-            {format(addHours(new Date(data.publishDate), 9), "yyyy.MM.dd")}
-          </div>
           <div className={styles.infoCategory}>
             {data.category.map((c) => (
               <p key={c} className={styles.infoCategoryItem}>
                 {c}
               </p>
             ))}
+          </div>
+          <div className={styles.infoDate}>
+            {format(addHours(new Date(data.publishDate), 9), "yyyy.MM.dd")}
           </div>
         </div>
       </div>
